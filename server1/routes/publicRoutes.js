ac0b93db6a5f802d33def965da4f1bf74fdcd39e
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-12-01 15:57:42 +0530

    dekete

--- server1/routes/publicRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPublicAstrologers, getBanners, getOffers } = require('../controllers/publicController');
-
-router.get('/astrologers', getPublicAstrologers);
-router.get('/banners', getBanners);
-router.get('/offers', getOffers);
-
-module.exports = router;
--- server1/routes/walletRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addMoney, getBalance } = require('../controllers/walletController');
-const auth = require('../middleware/auth');
-
-router.post('/add', auth, addMoney);
-router.get('/balance', auth, getBalance);
-
-module.exports = router;
