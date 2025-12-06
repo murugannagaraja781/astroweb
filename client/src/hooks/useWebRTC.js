759f397945d7a4f6543aed53e1e8dcb7a8dadf68
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-12-07 02:13:06 +0530

    Update useWebRTC.js

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -3,7 +3,12 @@ import { useState, useEffect, useRef, useCallback } from 'react';
 const ICE_SERVERS = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
-        { urls: 'stun:global.stun.twilio.com:3478' }
+        { urls: 'stun:global.stun.twilio.com:3478' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        { urls: 'stun:stun.services.mozilla.com' }
     ]
 };
 
